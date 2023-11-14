01a4e853710b591e5a6897f7b016d186a1970d37
Chibuike <chibuike.dmn@gmail.com>
2023-11-14 14:45:27 +0000

    feat : creado mertodos run y plantatr verduras en clase Granjero

--- src/net/salesianos/Granjero.java
+++ src/net/salesianos/Granjero.java
@@ -2,27 +2,38 @@ package net.salesianos;
 
 import java.util.Random;
 
-public class Granjero extends Almacen implements Runnable{
-              private String nombre;
-              private Almacen almacen;
-              private String[] verduras = {"lechuga", "repollo", "cebolla", "espinaca", "patata", "apio", "espárrago", "rábano", "brócoli", "alcachofa", "tomate", "pepino", "berenjena", "zanahoria", "judía verde"};
+public class Granjero extends Thread {
+    private String nombre;
+    private int cantidadVerdurasPlantar;
+    private Almacen almacen;
+    private static final int MAX_TIEMPO_CRECIMIENTO = 5000;
 
+    public Granjero(String nombre, int cantidadVerdurasPlantar, Almacen almacen) {
+        this.nombre = nombre;
+        this.cantidadVerdurasPlantar = cantidadVerdurasPlantar;
+        this.almacen = almacen;
+    }
 
+    @Override
+    public void run() {
+        for (int i = 0; i < cantidadVerdurasPlantar; i++) {
+            try {
+                Verduras verdura = plantarVerdura();
+                almacen.depositarVerdura(verdura);
+                sleep(verdura.getTiempoCrecimiento());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-              public Granjero(String nombre, Almacen almacen) {
-                            this.nombre = nombre;
-                            this.almacen = almacen;
-              }
+    private Verduras plantarVerdura() {
+        String[] tiposVerduras = {"lettuce", "cabbage", "onion", "spinach", "potato", "celery", "asparagus", "radish",
+                "broccoli", "artichoke", "tomato", "cucumber", "eggplant", "carrot", "green bean"};
 
-
-              @Override
-              public void run() {
-                                  
-
-
-
-
-              
-              }
-              
+        Random random = new Random();
+        String tipo = tiposVerduras[random.nextInt(tiposVerduras.length)];
+        int tiempoCrecimiento = random.nextInt(MAX_TIEMPO_CRECIMIENTO);
+        return new Verduras(tipo, nombre, tiempoCrecimiento);
+    }
 }
