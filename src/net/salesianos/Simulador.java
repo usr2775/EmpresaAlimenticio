8bdd9131ce0da5a376b556f1ffe17833950bdd83
Chibuike <chibuike.dmn@gmail.com>
2023-11-14 18:39:51 +0000

    feat: realizado prueba uno

--- src/net/salesianos/Simulador.java
+++ src/net/salesianos/Simulador.java
@@ -4,24 +4,18 @@ public class Simulador {
     public static void main(String[] args) {
         Almacen almacen = new Almacen(5);
 
-        Granjero paco = new Granjero("Paco", 10, almacen);
-        Granjero ramon = new Granjero("Ramon", 10, almacen);
+        Granjero paco = new Granjero("Paco", 5, almacen);
 
         Cliente faustino = new Cliente("Faustino", 5, almacen, 50);
-        Cliente gentleman = new Cliente("Mr. Gentleman", 10, almacen, 50);
-        Cliente loquendo = new Cliente("Loquendo", 5, almacen, 50);
 
         paco.start();
-        ramon.start();
         faustino.start();
-        gentleman.start();
-        loquendo.start();
+     
 
         try {
             paco.join();
             faustino.join();
-            gentleman.join();
-            loquendo.join();
+         
 
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
