f5365c5818ae2399bca919da167c979d6fd97e64
Chibuike <chibuike.dmn@gmail.com>
2023-11-14 14:47:23 +0000

    feat: creado clase Almacen  para depositar y consumir

--- src/net/salesianos/Almacen.java
+++ src/net/salesianos/Almacen.java
@@ -1,18 +1,33 @@
 package net.salesianos;
 
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Almacen {
-              private int capacidad;
-              private int espaciosDisponibles;
-              private boolean hayHueco;
+    private int capacidad;
+    private Queue<Verduras> verduras;
 
- public Almacen(int capacidad) {
-              this.capacidad = capacidad;
-              this.espaciosDisponibles = capacidad;
-              this.hayHueco = false;
+    public Almacen(int capacidad) {
+        this.capacidad = capacidad;
+        this.verduras = new LinkedList<>();
     }
 
-              
-} 
-              
+    public synchronized void depositarVerdura(Verduras verdura) throws InterruptedException {
+        while (verduras.size() >= capacidad) {
+            wait();
+        }
+        verduras.add(verdura);
+        System.out.println("Nueva verdura (" + verdura.getTipo() + ") producida por " + verdura.getGranjero());
+        notifyAll();
+    }
+
+    public synchronized Verduras consumirVerdura() throws InterruptedException {
+        while (verduras.isEmpty()) {
+            wait();
+        }
+        Verduras verdura = verduras.poll();
+        System.out.println(verdura.getTipo() + " consumida por " + Thread.currentThread().getName());
+        notifyAll();
+        return verdura;
+    }
+}
